e99650f9117c4fe9f43c700dc6246281ad0ba4c3
ignmiz <ignmiz@gmail.com>
2017-01-31 15:45:38 +0100

    Implemented flags for discrete component of model

--- utils/atcflags.h
+++ utils/atcflags.h
@@ -3,51 +3,83 @@
 
 namespace ATC {
 
-enum ATCDialogType
-{
-    TextConsole, Default
-};
+    enum ATCDialogType
+    {
+        TextConsole, Default
+    };
 
-enum AircraftType
-{
-    Civil, Military
-};
+    enum AircraftType
+    {
+        Civil, Military
+    };
 
-enum EngineType
-{
-    Jet, Turboprop, Piston
-};
+    enum EngineType
+    {
+        Jet, Turboprop, Piston
+    };
 
-enum WakeCategory
-{
-    J, H, M, L
-};
+    enum WakeCategory
+    {
+        J, H, M, L
+    };
 
-enum FlightRules
-{
-    IFR, VFR, SVFR
-};
+    enum FlightRules
+    {
+        IFR, VFR, SVFR
+    };
 
-enum NavMode
-{
-    Nav, Hdg
-};
+    enum NavMode
+    {
+        Nav, Hdg
+    };
 
-enum TagType
-{
-    Short, Full
-};
+    enum TagType
+    {
+        Short, Full
+    };
 
-enum AngularUnits
-{
-    Deg, Rad
-};
+    enum AngularUnits
+    {
+        Deg, Rad
+    };
 
-enum SimCreationMode
-{
-    New, Edit
-};
+    enum SimCreationMode
+    {
+        New, Edit
+    };
+}
 
+namespace BADA
+{
+    enum AccelerationMode
+    {
+        Accelerate, Decelerate, Constant
+    };
+
+    enum ClimbMode
+    {
+        Climb, Descend, Level
+    };
+
+    enum SpeedHoldMode
+    {
+        CAS, Mach
+    };
+
+    enum FlightPhase
+    {
+        UpperDescent, LowerDescent, Approach, Landing
+    };
+
+    enum ReducedPowerMode
+    {
+        On, Off
+    };
+
+    enum TroposphereMode
+    {
+        Low, High
+    };
 }
 
 
