d1796e44f658e0c53f3231605f36639c14fc2408
ignmiz <ignmiz@gmail.com>
2017-01-31 12:56:55 +0100

    Implemented GPF const extract

--- utils/atcconst.cpp
+++ utils/atcconst.cpp
@@ -18,3 +18,37 @@ const double ATCConst::WGS84_RADIUS = 6378137;
 const double ATCConst::WGS84_FIRST_ECCENTRICITY = 0.08181919092890624;
 
 const double ATCConst::SECTOR_SHRINK_FACTOR = 2;
+
+const double ATCConst::ACC_LONG_MAX = 2;    //ftps^2
+const double ATCConst::ACC_NORM_MAX = 5;    //ftps^2
+
+const double ATCConst::NOM_BANK_ANGLE = 30;
+
+const double ATCConst::C_DES_EXP = 1.6;
+
+const double ATCConst::C_TH_TO = 1.2;
+const double ATCConst::C_TH_CR = 0.95;
+
+const double ATCConst::C_V_MIN = 1.3;
+const double ATCConst::C_V_MIN_TO = 1.2;
+
+const double ATCConst::V_CL1 = 5;
+const double ATCConst::V_CL2 = 10;
+const double ATCConst::V_CL3 = 30;
+const double ATCConst::V_CL4 = 60;
+const double ATCConst::V_CL5 = 80;
+const double ATCConst::V_CL6 = 20;
+const double ATCConst::V_CL7 = 30;
+const double ATCConst::V_CL8 = 35;
+
+const double ATCConst::V_DES1 = 5;
+const double ATCConst::V_DES2 = 10;
+const double ATCConst::V_DES3 = 20;
+const double ATCConst::V_DES4 = 50;
+const double ATCConst::V_DES5 = 5;
+const double ATCConst::V_DES6 = 10;
+const double ATCConst::V_DES7 = 20;
+
+const double ATCConst::C_RED_PISTON = 0;
+const double ATCConst::C_RED_TURBO = 0.25;
+const double ATCConst::C_RED_JET = 0.15;
--- utils/atcconst.h
+++ utils/atcconst.h
@@ -8,6 +8,7 @@ class ATCConst
 public:
     ATCConst();
 
+    //General
     static const double PI;
 
     static const double DEG_2_RAD;
@@ -15,6 +16,7 @@ public:
 
     static const double NM_2_M;
 
+    //Scene & Display
     static const double AVG_DECLINATION;
 
     static const double WGS84_RADIUS;
@@ -28,7 +30,43 @@ public:
     static const int SCENE_HEIGHT = DISPLAY_HEIGHT * SCENE_FACTOR;
     static const double SECTOR_SHRINK_FACTOR;
 
+    //Simulation Performance
     static const qint64 DT = 100 * 1000000;
+
+    //BADA GPF Parameters Extract
+    static const double ACC_LONG_MAX;
+    static const double ACC_NORM_MAX;
+
+    static const double NOM_BANK_ANGLE;
+
+    static const double C_DES_EXP;
+
+    static const double C_TH_TO;
+    static const double C_TH_CR;
+
+    static const double C_V_MIN;
+    static const double C_V_MIN_TO;
+
+    static const double V_CL1;
+    static const double V_CL2;
+    static const double V_CL3;
+    static const double V_CL4;
+    static const double V_CL5;
+    static const double V_CL6;
+    static const double V_CL7;
+    static const double V_CL8;
+
+    static const double V_DES1;
+    static const double V_DES2;
+    static const double V_DES3;
+    static const double V_DES4;
+    static const double V_DES5;
+    static const double V_DES6;
+    static const double V_DES7;
+
+    static const double C_RED_PISTON;
+    static const double C_RED_TURBO;
+    static const double C_RED_JET;
 };
 
 #endif // ATCCONST_H
